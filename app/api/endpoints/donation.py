189f0fdf5553ee9266b04c603d67dbc289e39962
oitczvovich <63419396+oitczvovich@users.noreply.github.com>
2023-03-08 19:27:43 +0300

    first

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -4,11 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
 from app.core.user import current_superuser, current_user
-
-from app.schemas.donation import (
-    DonationBase,
-    DonationUserDB
-)
+from app.schemas.donation import DonationBase, DonationUserDB
 from app.crud.donation import donation_crud
 from app.models import User
 from app.services.investments import investment
